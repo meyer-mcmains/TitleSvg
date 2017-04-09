4c9a0eda225ec803c8cc1b5f125a795e6ac8a852
Meyer McMains <meyer.mcmains@gmail.com>
2017-04-09 16:59:48 -0400

    Initial commit of all files

---
+++ TitleSvg/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace TitleSvg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.openFileButton = new MetroFramework.Controls.MetroButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.titleLabel = new MetroFramework.Controls.MetroLabel();
+            this.titleBox = new MetroFramework.Controls.MetroTextBox();
+            this.changeButton = new MetroFramework.Controls.MetroButton();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.back = new MetroFramework.Controls.MetroButton();
+            this.forward = new MetroFramework.Controls.MetroButton();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.file = new MetroFramework.Controls.MetroLabel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.wikiLabel = new MetroFramework.Controls.MetroLabel();
+            this.linkLabel = new MetroFramework.Controls.MetroLabel();
+            this.wikiText = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.wikiChangeButton = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.openFileButton.Location = new System.Drawing.Point(3, 3);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(115, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.openFileButton.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.flowLayoutPanel1.Controls.Add(this.openFileButton);
+            this.flowLayoutPanel1.Controls.Add(this.titleLabel);
+            this.flowLayoutPanel1.Controls.Add(this.titleBox);
+            this.flowLayoutPanel1.Controls.Add(this.changeButton);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.wikiChangeButton);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 60);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(613, 329);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.titleLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.titleLabel.Location = new System.Drawing.Point(3, 29);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(55, 25);
+            this.titleLabel.Style = MetroFramework.MetroColorStyle.Blue;
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Title:";
+            this.titleLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.titleLabel.UseStyleColors = true;
+            // 
+            // titleBox
+            // 
+            this.titleBox.Enabled = false;
+            this.titleBox.Location = new System.Drawing.Point(3, 57);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(115, 23);
+            this.titleBox.TabIndex = 2;
+            this.titleBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.titleBox.TextChanged += new System.EventHandler(this.titleBox_TextChanged);
+            // 
+            // changeButton
+            // 
+            this.changeButton.Enabled = false;
+            this.changeButton.Location = new System.Drawing.Point(3, 86);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(115, 23);
+            this.changeButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.changeButton.TabIndex = 4;
+            this.changeButton.Text = "Add Metadata";
+            this.changeButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.back);
+            this.flowLayoutPanel2.Controls.Add(this.forward);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 115);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(116, 39);
+            this.flowLayoutPanel2.TabIndex = 6;
+            // 
+            // back
+            // 
+            this.back.Enabled = false;
+            this.back.Location = new System.Drawing.Point(0, 3);
+            this.back.Margin = new System.Windows.Forms.Padding(0, 3, 6, 3);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(52, 36);
+            this.back.TabIndex = 1;
+            this.back.Text = "<";
+            this.back.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // forward
+            // 
+            this.forward.Enabled = false;
+            this.forward.Location = new System.Drawing.Point(64, 3);
+            this.forward.Margin = new System.Windows.Forms.Padding(6, 3, 0, 3);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(52, 36);
+            this.forward.Style = MetroFramework.MetroColorStyle.Blue;
+            this.forward.TabIndex = 0;
+            this.forward.Text = ">";
+            this.forward.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.Controls.Add(this.metroLabel1);
+            this.flowLayoutPanel3.Controls.Add(this.file);
+            this.flowLayoutPanel3.Controls.Add(this.webBrowser1);
+            this.flowLayoutPanel3.Controls.Add(this.wikiLabel);
+            this.flowLayoutPanel3.Controls.Add(this.linkLabel);
+            this.flowLayoutPanel3.Controls.Add(this.wikiText);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(125, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(489, 326);
+            this.flowLayoutPanel3.TabIndex = 9;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(51, 25);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "File: ";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel1.UseStyleColors = true;
+            // 
+            // file
+            // 
+            this.file.AutoSize = true;
+            this.file.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.file.Location = new System.Drawing.Point(60, 0);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(0, 0);
+            this.file.TabIndex = 1;
+            this.file.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.file.UseStyleColors = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(3, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(483, 218);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // wikiLabel
+            // 
+            this.wikiLabel.AutoSize = true;
+            this.wikiLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.wikiLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.wikiLabel.Location = new System.Drawing.Point(3, 249);
+            this.wikiLabel.Name = "wikiLabel";
+            this.wikiLabel.Size = new System.Drawing.Size(55, 25);
+            this.wikiLabel.TabIndex = 10;
+            this.wikiLabel.Text = "Wiki:";
+            this.wikiLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.wikiLabel.UseStyleColors = true;
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.linkLabel.Location = new System.Drawing.Point(64, 249);
+            this.linkLabel.MinimumSize = new System.Drawing.Size(0, 25);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(0, 25);
+            this.linkLabel.TabIndex = 12;
+            this.linkLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.linkLabel.UseStyleColors = true;
+            this.linkLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.linkLabel_MouseClick);
+            // 
+            // wikiText
+            // 
+            this.wikiText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.wikiText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.wikiText.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.wikiText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wikiText.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.wikiText.Location = new System.Drawing.Point(3, 277);
+            this.wikiText.Name = "wikiText";
+            this.wikiText.Size = new System.Drawing.Size(482, 46);
+            this.wikiText.TabIndex = 11;
+            this.wikiText.Text = "";
+            this.wikiText.TextChanged += new System.EventHandler(this.wikiText_TextChanged);
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(620, 3);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel4.TabIndex = 12;
+            // 
+            // wikiChangeButton
+            // 
+            this.wikiChangeButton.Enabled = false;
+            this.wikiChangeButton.Location = new System.Drawing.Point(3, 160);
+            this.wikiChangeButton.Name = "wikiChangeButton";
+            this.wikiChangeButton.Size = new System.Drawing.Size(115, 23);
+            this.wikiChangeButton.TabIndex = 13;
+            this.wikiChangeButton.Text = "Change Wiki";
+            this.wikiChangeButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.wikiChangeButton.Click += new System.EventHandler(this.wikiChangeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 409);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Title SVG";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroButton openFileButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private MetroFramework.Controls.MetroTextBox titleBox;
+        private MetroFramework.Controls.MetroLabel titleLabel;
+        private MetroFramework.Controls.MetroButton changeButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private MetroFramework.Controls.MetroButton forward;
+        private MetroFramework.Controls.MetroButton back;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel file;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private MetroFramework.Controls.MetroLabel wikiLabel;
+        private System.Windows.Forms.RichTextBox wikiText;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private MetroFramework.Controls.MetroLabel linkLabel;
+        private MetroFramework.Controls.MetroButton wikiChangeButton;
+    }
+}
+
---
+++ TitleSvg/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Drawing;
+using System.Dynamic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using MetroFramework.Forms;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace TitleSvg
+{
+
+    //This program uses the Metro Framework https://github.com/thielj/MetroFramework
+    public partial class Form1 : MetroForm
+    {
+        private int index = 1;
+        private string path = null;
+        private bool hastitle = true;
+        private string text;
+
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("about:blank");
+            webBrowser1.Document.BackColor = Color.FromArgb(30, 30, 30);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            var open = new OpenFileDialog();
+            open.Title = "Find SVG";
+            open.Filter = "SVG files (*.svg)|*.svg";
+            open.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                path = open.FileName;
+                index = setIndex();
+                forward.Enabled = true;
+                back.Enabled = true;
+                titleBox.Enabled = true;
+                ReadSvg();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            webBrowser1.Width = (flowLayoutPanel1.Width - flowLayoutPanel2.Width - 20);
+            wikiText.Height = (flowLayoutPanel1.Height / 3);
+            webBrowser1.Height = (flowLayoutPanel1.Height - wikiText.Height - wikiLabel.Height - metroLabel1.Height - 12);
+            wikiText.Width = (flowLayoutPanel1.Width - flowLayoutPanel2.Width - 20);
+            linkLabel.Width = (webBrowser1.Width - (wikiLabel.Width + 5));
+            file.Width = (webBrowser1.Width - (metroLabel1.Width + 5));
+        }
+
+        private void titleBox_TextChanged(object sender, EventArgs e)
+        {
+            if (titleBox.Text.Length > 0 && path != null)
+            {
+                changeButton.Enabled = true;
+            }
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            var doc = XDocument.Load(path);
+            if (hastitle == true)
+            {
+                doc.Root.Element("title").Value = titleBox.Text;
+                getWiki();
+                doc.Root.Element("wiki").Value = wikiText.Text;
+                doc.Root.Element("link").Value = linkLabel.Text;
+            }
+            else
+            {
+                XObject title = new XElement("title", titleBox.Text);
+                doc.Root.Add(title);
+                getWiki();
+                XObject wiki = new XElement("wiki", wikiText.Text);
+                doc.Root.Add(wiki);
+                XObject link = new XElement("link", linkLabel.Text);
+                doc.Root.Add(link);
+                hastitle = true;
+            }
+            doc.Save(path);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            if (index > 1 && path != null)
+            {
+                index --;
+                Parse();
+                wikiText.Text = "";
+                linkLabel.Text = "";
+                if(readFile() == false)
+                {
+                    ReadSvg();
+                }
+            }
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            if (path != null)
+            {
+                index++;
+                Parse();
+                wikiText.Text = "";
+                linkLabel.Text = "";
+                if (readFile() == false)
+                {
+                    ReadSvg();
+                }
+            }
+        }
+
+        private void ReadSvg()
+        {
+            try
+            {
+                titleBox.Enabled = true;
+                file.Text = path;
+                hastitle = true;
+
+                webBrowser1.DocumentText =
+                    "<!DOCTYPE html><head><meta http-equiv='X-UA-Compatible' content='IE=9'/></head><html lang='en'>" +
+                    "<body style='background-color: rgb(30, 30, 30);'>" +
+                    "<div style='position: fixed; text-align: center; left: 0; right: 0; top: 0; bottom: 0;'>" +
+                    "<img style='height: 100%; width: 100%;' src = '" + path + "'>" +
+                    "<p style='color: white; width: 100%; height: 100%; font-size: 10vw; font-family: sans-serif;'>" + text + "</p></div></body></html>";
+
+
+                var doc = XDocument.Load(path);
+                titleBox.Text = doc.Root.Element("title").Value;
+                wikiText.Text = doc.Root.Element("wiki").Value;
+                linkLabel.Text = doc.Root.Element("link").Value;
+                changeButton.Enabled = false;
+            }
+            catch (Exception)
+            {
+                titleBox.Text = "Add A Title";
+                changeButton.Enabled = false;
+                hastitle = false;
+            }
+        }
+
+        private void Parse()
+        {
+            char[] delims = {'\\', '.'};
+            var words = path.Split(delims);
+            var regex = new Regex(@"^\d+$");
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (regex.IsMatch(words[i]))
+                {
+                    words[i] = index.ToString();
+                    words[i] = words[i] + ".";
+                    path = string.Join("", words);
+                    break;
+                }
+                words[i] = words[i] + "\\";
+            }
+        }
+
+        private int setIndex()
+        {
+            char[] delims = { '\\', '.' };
+            var words = path.Split(delims);
+            var regex = new Regex(@"^\d+$");
+            int index = 1;
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (regex.IsMatch(words[i]))
+                {
+                    index = Convert.ToInt16(words[i]);
+                    break;
+                }
+            }
+            return index;
+        }
+
+        private void getWiki()
+        {
+            string format = Regex.Replace(titleBox.Text, "[_]", "_");
+            string link = "https://en.wikipedia.org/wiki/" + titleBox.Text;
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument htmlDoc = web.Load(link);
+
+            try
+            {
+                if (htmlDoc.DocumentNode != null)
+                {
+                    linkLabel.Text = link;
+                    HtmlAgilityPack.HtmlNode paragraph = htmlDoc.DocumentNode.SelectSingleNode("//*[@id='mw-content-text']/p[1]");
+                    wikiText.Text = Regex.Replace(paragraph.InnerText, "[[(\\d)+]+]", "");   
+                }
+                else
+                {
+                    wikiText.ForeColor = Color.Red;
+                    wikiText.Text = "Error Reading Wiki.";
+                }
+            }
+            catch (Exception)
+            {
+                wikiText.ForeColor = Color.Red;
+                wikiText.Text = "Wikipedia does not have an article with this exact name.";
+            }
+
+            wikiChangeButton.Enabled = false;
+        }
+
+        private void linkLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel.Text);
+        }
+
+        private void wikiChangeButton_Click(object sender, EventArgs e)
+        {
+            var doc = XDocument.Load(path);
+            doc.Root.Element("wiki").Value = wikiText.Text;
+            doc.Save(path);
+            wikiChangeButton.Enabled = false;
+        }
+
+        private void wikiText_TextChanged(object sender, EventArgs e)
+        {
+            wikiChangeButton.Enabled = true;
+        }
+
+        private bool readFile()
+        {
+            bool status = false;
+            try
+            {
+                File.ReadAllText(path);
+            }
+            catch (Exception)
+            {
+                string format = Regex.Replace(path, @"\b.svg\b", ".txt");
+                try
+                {
+                    StreamReader txt = new StreamReader(format);
+                    file.Text = format;
+                    titleBox.Text = "";
+                    titleBox.Enabled = false;
+                    text = txt.ReadLine();
+                    webBrowser1.DocumentText = "<!DOCTYPE html><head><meta http-equiv='X-UA-Compatible' content='IE=9'/></head><html lang='en'>" +
+                        "<body style='background-color: rgb(30, 30, 30);'>" +
+                        "<div style='position: fixed; text-align: center; left: 0; right: 0; top: 0; bottom: 0;'>" +
+                        "<p style='color: white; width: 100%; height: 100%; font-size: 10vw; font-family: sans-serif;'>" + text + "</p></div></body></html>";
+                    status = true;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return status;
+        }
+    }
+}
